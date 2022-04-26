74becae021aed83240fce57d5be5ced2850960d9
Andrew Somers <42009457+Myndex@users.noreply.github.com>
2022-04-25 18:53:46 -0700

    Fixed "lower case" bug

--- src/colorparsley.js
+++ src/colorparsley.js
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 /** @preserve
 /////    CoLoR PaRsLeY  🎨 🌿  a simple set of color parsing thingies!
-/////           Beta 0.1.4   Revision date: April 15, 2022
+/////           Beta 0.1.5   Revision date: April 25, 2022
 /////
 /////    Functions to parse color values and return array
 /////    Copyright © 2019-2022 by Andrew Somers. All Rights Reserved.
@@ -33,7 +33,7 @@
 
 
 ////////////////////////////////////////////////////////////////////////////////
-/////  BEGIN COLOR PARSLEY 0.1.4  BLOCK  \/////////////////////////////////////
+/////  BEGIN COLOR PARSLEY 0.1.5  BLOCK  \/////////////////////////////////////
 ////                                      \///////////////////////////////////
 ///                                        \/////////////////////////////////
 
@@ -52,20 +52,30 @@ function colorParsley (colorIn) {
     } else if (typeof colorIn === 'object') {
        if (Array.isArray(colorIn)) {
           return colorIn;
-       } else if (colorIn.r || colorIn.red){ // validate object & return array
+       } else if (!isNaN(colorIn.r) || !isNaN(colorIn.red)){
+              // validate object & return array
          let objArray = [0,0,0,'',false,'unknown'];
-                       // takes object with r g b or red green blue etc...
-         objArray[0] = (colorIn.r)?colorIn.r:(colorIn.red)?colorIn.red:0;
-         objArray[1] = (colorIn.g)?colorIn.g:(colorIn.green)?colorIn.green:0;
-         objArray[2] = (colorIn.b)?colorIn.b:(colorIn.blue)?colorIn.blue:0;
-         objArray[3] = (colorIn.a)?colorIn.a:(colorIn.alpha)?colorIn.alpha:'';
-         objArray[4] = true;
-
+             // takes object with r g b or red green blue etc...
+         objArray[0] = (colorIn.r)?colorIn.r:(colorIn.red)?
+                        colorIn.red:false;
+         objArray[1] = (colorIn.g)?colorIn.g:(colorIn.green)?
+                        colorIn.green:false;
+         objArray[2] = (colorIn.b)?colorIn.b:(colorIn.blue)?
+                        colorIn.blue:false;
+         objArray[3] = (colorIn.a)?colorIn.a:(colorIn.alpha)?
+                        colorIn.alpha:'';
+         objArray[4] = (objArray[0]&&objArray[1]&&objArray[2]) ? true : false;
+         objArray[5] =
+            (colorIn.space)?colorIn.space:
+            (colorIn.colorSpace)?colorIn.colorSpace:
+            (colorIn.colorspace)?colorIn.colorspace:
+            'unknown';
+         
          return objArray;
        }
     }; // End if statement
         console.log('colorParsley error: invalid input')
-        return [0,0,0,'',false,'inputError'] // throw 'InvalidInput' // return error 
+        return [0,0,0,0,false,'inputError'] // throw 'InvalidInput' // return error 
 };
 
 
@@ -79,19 +89,41 @@ function parseString (colorString = '#abcdef') {
                    // strip junk and make a clean string (replace unmatched)
                   // This retains all alphanumeric and , . # % ( ) /
     colorString = colorString.replace(/[^\w,.#%()\/ -]/g,'');
+    colorString = colorString.toLowerCase();   // set lowercase
     
-    this.isValid = false; // validation flag, in 5th array element.
-    this.type = 'sRGB'; // Default colorspace flag in 6th element.
+    this.isValid = false; // validation flag, in array element [4]
+    this.type = 'sRGB'; // Default colorspace flag in element [5]
     
-        // test for named color before iterating array (optimization)
-    if (colorString.match(/^(?:(?!rgb|l.h|hs.|#).+)(?=[G-Z])/i)) {
+        // test for named color before iterating array (is optimization needed?)
+    if (colorString.match(/^(?:(?!rgb|l.h|hs|col|\d|#).{0,4})(?=[g-z])/)) {
 
-      colorString = colorString.toLowerCase();   // set lowercase
-
-      ///// CSS4 NAMED COLORS plus GREYS and GRAYS /////////////
+      ///// CSS4 NAMED COLORS plus a bonus set of GREYS and GRAYS /////////////
 
       // See if name is matched and overwrite the input
-      let namedColors = { gray1:'111111',gray2:'222222',gray3:'333333',gray4:'444444',gray5:'555555',gray6:'666666',gray7:'777777',gray8:'888888',gray9:'999999',graya:'aaaaaa',grayb:'bbbbbb',grayc:'cccccc',grayd:'dddddd',graye:'eeeeee',grey1:'111111',grey2:'222222',grey3:'333333',grey4:'444444',grey5:'555555',grey6:'666666',grey7:'777777',grey8:'888888',grey9:'999999',greya:'aaaaaa',greyb:'bbbbbb',greyc:'cccccc',greyd:'dddddd',greye:'eeeeee',aliceblue:'f0f8ff',antiquewhite:'faebd7',aqua:'00ffff',aquamarine:'7fffd4',azure:'f0ffff',beige:'f5f5dc',bisque:'ffe4c4',black:'000000',blanchedalmond:'ffebcd',blue:'0000ff',blueviolet:'8a2be2',brown:'a52a2a',burlywood:'deb887',cadetblue:'5f9ea0',chartreuse:'7fff00',chocolate:'d2691e',coral:'ff7f50',cornflowerblue:'6495ed',cornsilk:'fff8dc',crimson:'dc143c',cyan:'00ffff',darkblue:'00008b',darkcyan:'008b8b',darkgoldenrod:'b8860b',darkgray:'a9a9a9',darkgreen:'006400',darkgrey:'a9a9a9',darkkhaki:'bdb76b',darkmagenta:'8b008b',darkolivegreen:'556b2f',darkorange:'ff8c00',darkorchid:'9932cc',darkred:'8b0000',darksalmon:'e9967a',darkseagreen:'8fbc8f',darkslateblue:'483d8b',darkslategray:'2f4f4f',darkslategrey:'2f4f4f',darkturquoise:'00ced1',darkviolet:'9400d3',deeppink:'ff1493',deepskyblue:'00bfff',dimgray:'696969',dimgrey:'696969',dodgerblue:'1e90ff',firebrick:'b22222',floralwhite:'fffaf0',forestgreen:'228b22',fuchsia:'ff00ff',gainsboro:'dcdcdc',ghostwhite:'f8f8ff',gold:'ffd700',goldenrod:'daa520',gray:'808080',green:'008000',greenyellow:'adff2f',grey:'808080',honeydew:'f0fff0',hotpink:'ff69b4',indianred:'cd5c5c',indigo:'4b0082',ivory:'fffff0',khaki:'f0e68c',lavender:'e6e6fa',lavenderblush:'fff0f5',lawngreen:'7cfc00',lemonchiffon:'fffacd',lightblue:'add8e6',lightcoral:'f08080',lightcyan:'e0ffff',lightgoldenrodyellow:'fafad2',lightgray:'d3d3d3',lightgreen:'90ee90',lightgrey:'d3d3d3',lightpink:'ffb6c1',lightsalmon:'ffa07a',lightseagreen:'20b2aa',lightskyblue:'87cefa',lightslategray:'778899',lightslategrey:'778899',lightsteelblue:'b0c4de',lightyellow:'ffffe0',lime:'00ff00',limegreen:'32cd32',linen:'faf0e6',magenta:'ff00ff',maroon:'800000',mediumaquamarine:'66cdaa',mediumblue:'0000cd',mediumorchid:'ba55d3',mediumpurple:'9370db',mediumseagreen:'3cb371',mediumslateblue:'7b68ee',mediumspringgreen:'00fa9a',mediumturquoise:'48d1cc',mediumvioletred:'c71585',midnightblue:'191970',mintcream:'f5fffa',mistyrose:'ffe4e1',moccasin:'ffe4b5',navajowhite:'ffdead',navy:'000080',oldlace:'fdf5e6',olive:'808000',olivedrab:'6b8e23',orange:'ffa500',orangered:'ff4500',orchid:'da70d6',palegoldenrod:'eee8aa',palegreen:'98fb98',paleturquoise:'afeeee',palevioletred:'db7093',papayawhip:'ffefd5',peachpuff:'ffdab9',peru:'cd853f',pink:'ffc0cb',plum:'dda0dd',powderblue:'b0e0e6',purple:'800080',rebeccapurple:'663399',red:'ff0000',rosybrown:'bc8f8f',royalblue:'4169e1',saddlebrown:'8b4513',salmon:'fa8072',sandybrown:'f4a460',seagreen:'2e8b57',seashell:'fff5ee',sienna:'a0522d',silver:'c0c0c0',skyblue:'87ceeb',slateblue:'6a5acd',slategray:'708090',slategrey:'708090',snow:'fffafa',springgreen:'00ff7f',steelblue:'4682b4',tan:'d2b48c',teal:'008080',thistle:'d8bfd8',tomato:'ff6347',turquoise:'40e0d0',violet:'ee82ee',wheat:'f5deb3',white:'ffffff',whitesmoke:'f5f5f5',yellow:'ffff00',yellowgreen:'9acd32'};
+      let namedColors = {
+      gray0:'000000',gray1:'111111',gray2:'222222',gray3:'333333',gray4:'444444',gray5:'555555',gray6:'666666',gray7:'777777',gray8:'888888',gray9:'999999',graya:'aaaaaa',grayb:'bbbbbb',grayc:'cccccc',grayd:'dddddd',graye:'eeeeee',grayf:'ffffff',midgray:'a0a0a0',
+      grey0:'000000',grey1:'111111',grey2:'222222',grey3:'333333',grey4:'444444',grey5:'555555',grey6:'666666',grey7:'777777',grey8:'888888',grey9:'999999',greya:'aaaaaa',greyb:'bbbbbb',greyc:'cccccc',greyd:'dddddd',greye:'eeeeee',greyf:'ffffff',midgrey:'a0a0a0',
+      aliceblue:'f0f8ff',antiquewhite:'faebd7',aqua:'00ffff',aquamarine:'7fffd4',azure:'f0ffff',
+      beige:'f5f5dc',bisque:'ffe4c4',black:'000000',blanchedalmond:'ffebcd',blue:'0000ff',blueviolet:'8a2be2',brown:'a52a2a',burlywood:'deb887',
+      cadetblue:'5f9ea0',chartreuse:'7fff00',chocolate:'d2691e',coral:'ff7f50',cornflowerblue:'6495ed',cornsilk:'fff8dc',crimson:'dc143c',cyan:'00ffff',
+      darkblue:'00008b',darkcyan:'008b8b',darkgoldenrod:'b8860b',darkgray:'a9a9a9',darkgreen:'006400',darkgrey:'a9a9a9',darkkhaki:'bdb76b',darkmagenta:'8b008b',darkolivegreen:'556b2f',darkorange:'ff8c00',darkorchid:'9932cc',darkred:'8b0000',darksalmon:'e9967a',darkseagreen:'8fbc8f',darkslateblue:'483d8b',darkslategray:'2f4f4f',darkslategrey:'2f4f4f',darkturquoise:'00ced1',darkviolet:'9400d3',deeppink:'ff1493',deepskyblue:'00bfff',dimgray:'696969',dimgrey:'696969',dodgerblue:'1e90ff',
+      firebrick:'b22222',floralwhite:'fffaf0',forestgreen:'228b22',fuchsia:'ff00ff',
+      gainsboro:'dcdcdc',ghostwhite:'f8f8ff',gold:'ffd700',goldenrod:'daa520',gray:'808080',green:'008000',greenyellow:'adff2f',grey:'808080',
+      honeydew:'f0fff0',hotpink:'ff69b4',
+      indianred:'cd5c5c',indigo:'4b0082',ivory:'fffff0',
+      khaki:'f0e68c',
+      lavender:'e6e6fa',lavenderblush:'fff0f5',lawngreen:'7cfc00',lemonchiffon:'fffacd',lightblue:'add8e6',lightcoral:'f08080',lightcyan:'e0ffff',lightgoldenrodyellow:'fafad2',lightgray:'d3d3d3',lightgreen:'90ee90',lightgrey:'d3d3d3',lightpink:'ffb6c1',lightsalmon:'ffa07a',lightseagreen:'20b2aa',lightskyblue:'87cefa',lightslategray:'778899',lightslategrey:'778899',lightsteelblue:'b0c4de',lightyellow:'ffffe0',lime:'00ff00',limegreen:'32cd32',linen:'faf0e6',
+      magenta:'ff00ff',maroon:'800000',mediumaquamarine:'66cdaa',mediumblue:'0000cd',mediumorchid:'ba55d3',mediumpurple:'9370db',mediumseagreen:'3cb371',mediumslateblue:'7b68ee',mediumspringgreen:'00fa9a',mediumturquoise:'48d1cc',mediumvioletred:'c71585',midnightblue:'191970',mintcream:'f5fffa',mistyrose:'ffe4e1',moccasin:'ffe4b5',
+      navajowhite:'ffdead',navy:'000080',
+      oldlace:'fdf5e6',olive:'808000',olivedrab:'6b8e23',orange:'ffa500',orangered:'ff4500',orchid:'da70d6',
+      palegoldenrod:'eee8aa',palegreen:'98fb98',paleturquoise:'afeeee',palevioletred:'db7093',papayawhip:'ffefd5',peachpuff:'ffdab9',peru:'cd853f',pink:'ffc0cb',plum:'dda0dd',powderblue:'b0e0e6',purple:'800080',
+      rebeccapurple:'663399',red:'ff0000',rosybrown:'bc8f8f',royalblue:'4169e1',
+      saddlebrown:'8b4513',salmon:'fa8072',sandybrown:'f4a460',seagreen:'2e8b57',seashell:'fff5ee',sienna:'a0522d',silver:'c0c0c0',skyblue:'87ceeb',slateblue:'6a5acd',slategray:'708090',slategrey:'708090',snow:'fffafa',springgreen:'00ff7f',steelblue:'4682b4',
+      tan:'d2b48c',teal:'008080',thistle:'d8bfd8',tomato:'ff6347',turquoise:'40e0d0',
+      violet:'ee82ee',
+      wheat:'f5deb3',white:'ffffff',whitesmoke:'f5f5f5',
+      yellow:'ffff00',yellowgreen:'9acd32'
+      };
 
       for (let key in namedColors) {
         if (colorString == key) {
@@ -252,7 +284,7 @@ function parseString (colorString = '#abcdef') {
     } else {
       this.isValid = false;
       console.log('colorParsley error: unable to parse string')
-      return [0,0,0,'',this.isValid,'parsleyError']  // throw 'InvalidString'
+      return [0,0,0,0,this.isValid,'parsleyError']  // throw 'InvalidString'
     }
 };
 
@@ -358,6 +390,6 @@ module.exports = { colorParsley, colorToHex, colorToRGB };
 
 ///\                                      //////////////////////////////////////
 ////\                                    //////////////////////////////////////
-/////\  END COLOR PARSLEY 0.1.4  BLOCK  //////////////////////////////////////
+/////\  END COLOR PARSLEY 0.1.5  BLOCK  //////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////
 
